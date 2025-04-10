d6faf57816772b1ee5bd4073d281302fb417eccb
jdslater17 <jdslater17@gmail.com>
2025-04-10 16:43:58 -0400

    Update gradebook.js

--- gradebook.js
+++ gradebook.js
@@ -1,1 +1,8 @@
-21
+function fetchGradeData() {
+  console.log("Fetching grade data...")
+}
+function populateGradebook(data) {
+  console.log("populate gradebook with data:", data);
+}
+const gradeData = fetchGradeData();
+populateGradebook(gradeData);
